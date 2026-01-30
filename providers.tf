4957135d7f22a4388d0607496bc9a46ddfe0d088
Chris Collis <collisdigital@users.noreply.github.com>
2026-01-30 15:08:45 +0000

    Fix Cloudflare dns resources

--- providers.tf
+++ providers.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.14.3"
 
   required_providers {
     hcloud = {
       source  = "hetznercloud/hcloud"
-      version = "~> 1.45"
+      version = "~> 1.60"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
