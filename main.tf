f0ef8bc8f4137203d5e5c03ee423ffd2e3393978
Chris Collis <collisdigital@users.noreply.github.com>
2026-01-30 15:41:39 +0000

    WIP

--- main.tf
+++ main.tf
@@ -5,17 +5,18 @@ data "hcloud_ssh_key" "main" {
 resource "hcloud_server" "devpush" {
   name         = "devpush"
   image        = "ubuntu-24.04"
-  server_type  = "cpx11"
+  server_type  = "cax11"
   location     = "nbg1"
   ssh_keys     = [data.hcloud_ssh_key.main.id]
   firewall_ids = [hcloud_firewall.devpush.id]
+  backups      = true
 
-  user_data = templatefile("${path.module}/cloud-config.yaml", {
+  user_data = templatefile("${path.module}/devpush-config.yaml", {
     ssh_public_key = data.hcloud_ssh_key.main.public_key
   })
 
   public_net {
-    ipv4_enabled = true
+    ipv4_enabled = false
     ipv6_enabled = true
   }
 }
