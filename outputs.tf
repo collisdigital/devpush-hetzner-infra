0664b0540712f2749b5a36b3021caf19b44b6ab6
Chris Collis <collisdigital@users.noreply.github.com>
2026-01-31 10:07:32 +0000

    Improve reliability of installation

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,24 @@
-output "server_ip" {
+output "server_ipv4" {
   value       = hcloud_primary_ip.devpush_ipv4.ip_address
-  description = "Public IP of the DevPush server"
+  description = "Public IPv4 address of the DevPush server"
+}
+
+output "server_ipv6" {
+  value       = hcloud_primary_ip.devpush_ipv6.ip_address
+  description = "Public IPv6 address of the DevPush server"
 }
 
 output "ssh_command" {
-  value       = "ssh devpush@devpush-direct.${var.domain_name}"
+  value       = "ssh -i <ssh_key_path> ${var.ssh_login_username}@${cloudflare_dns_record.devpush_direct_v4.name}.${var.domain_name}"
   description = "Command to SSH into the server"
 }
 
 output "devpush_url" {
-  value       = "https://devpush.${var.domain_name}"
+  value       = "https://${cloudflare_dns_record.devpush_app.name}.${var.domain_name}"
   description = "URL of the DevPush dashboard"
+}
+
+output "hetzner_location" {
+  value       = var.hetzner_location
+  description = "Hetzner datacenter location"
 }
